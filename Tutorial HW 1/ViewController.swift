2ba186b013a3082b8f108bd732d7df32693ad642
Taylor Thomas <22thotay@techs-macbook-pro-2.local>
2019-08-21 14:41:13 -0400

    Added in What up Nerd? No problems this time around.

--- Tutorial HW 1/ViewController.swift
+++ Tutorial HW 1/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let message = "What up, Nerd?"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(message)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated
+    }
 
 }
 
